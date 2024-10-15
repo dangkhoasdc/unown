e90be1c7c99e89e4db5e6b28f24edf9ec7cab302
Khoa Le Tan Dang <letan.dangkhoa@gmail.com>
2024-10-15 03:58:23 +0000

    add nvim-ufo

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,9 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+-- Folding settings
+vim.o.foldcolumn = "1"
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = -1
+vim.o.foldenable = true
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -50,7 +50,6 @@ return {
     end,
   },
 
-
   -- Undo
   {
     "mbbill/undotree",
@@ -64,6 +63,22 @@ return {
     cmd = "TodoQuickFix",
     lazy = false,
     opts = {}
+  },
+
+  -- folding
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
+    lazy = false,
+    config = function()
+      vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+      require('ufo').setup({
+        provider_selector = function(bufnr, filetype, buftype)
+          return { 'treesitter', 'indent' }
+        end
+      })
+    end,
   },
 
 }
