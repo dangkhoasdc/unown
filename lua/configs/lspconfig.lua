c5ca413807103aaa62ee7a944033e35463fb2402
Khoa <letan.dangkhoa@gmail.com>
2024-12-22 11:01:44 +0800

    add vale ls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "pylsp", "lua_ls", "gopls" }
+local servers = { "pylsp", "lua_ls", "gopls", "vale_ls"}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
