af72f70e9a2066a6493a816277194633a386fa7b
Khoa Le Tan Dang <letan.dangkhoa@gmail.com>
2024-12-23 06:41:06 +0000

    add dockerls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "pylsp", "lua_ls", "gopls", "vale_ls"}
+local servers = { "pylsp", "lua_ls", "gopls", "vale_ls", "dockerls"}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
