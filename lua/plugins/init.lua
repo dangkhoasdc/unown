40c9619f99f087503619046272cefb2e3e58ebd5
Khoa <letan.dangkhoa@gmail.com>
2025-01-02 11:41:24 +0800

    add nvim-lint

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,24 @@ return {
   },
 
   {
+    "mfussenegger/nvim-lint",
+    lazy=false,
+    config = function()
+      require('lint').linters_by_ft = {
+        make = { 'checkmake' }
+      }
+      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        callback = function()
+          local lint_status, lint = pcall(require, "lint")
+          if lint_status then
+            lint.try_lint()
+          end
+        end,
+      })
+    end,
+  },
+
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
