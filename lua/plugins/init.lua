79f52d638a8485de9e48530504599bc584f0f4fa
Khoa Le Tan Dang <letan.dangkhoa@gmail.com>
2025-02-26 01:55:00 +0000

    add noice for better UI

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -145,4 +145,21 @@ return {
       { "<leader>ga", ":lua require('neogen').generate()<CR>", desc = "[G]enerate [A]nnotations", noremap = true, silent = true }
     }
   },
+
+  -- UI
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  },
 }
