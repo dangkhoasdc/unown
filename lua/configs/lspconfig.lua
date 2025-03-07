e5b0b629f120b03853853b7397da26da4626da29
Khoa Le Tan Dang <letan.dangkhoa@gmail.com>
2025-03-07 04:20:57 +0000

    still need pylsp for autocomplete

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = {"lua_ls", "gopls", "vale_ls", "dockerls", "julials", "ruff"}
+local servers = { "pylsp", "lua_ls", "gopls", "vale_ls", "dockerls", "julials", "ruff"}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
