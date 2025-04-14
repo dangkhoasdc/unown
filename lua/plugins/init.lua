4b613bb46c49f2aaae4dc95ae9bd21eee5f764ea
Le Tan Dang Khoa <letan.dangkhoa@gmail.com>
2025-04-14 04:08:07 +0000

    add gopher.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -274,4 +274,16 @@ return {
     event = { "BufReadPost" },
     ft = { "json", "yaml" },
   },
+  -- Golang
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    -- branch = "develop"
+    -- (optional) will update plugin's deps on every update
+    build = function()
+      vim.cmd.GoInstallDeps()
+    end,
+    ---@type gopher.Config
+    opts = {},
+  },
 }
