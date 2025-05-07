1e1174cf86b17b4a3a6cb972ec91a3a9fba8fc83
Le Tan Dang Khoa <letan.dangkhoa@gmail.com>
2025-05-07 02:12:30 +0000

    feat: give Octo a try

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -258,6 +258,19 @@ return {
     end,
   },
 
+  {
+    "pwntester/octo.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    cmd = {"Octo"},
+    config = function()
+      require("octo").setup()
+    end,
+  },
+
   -- save and restore sessions
   {
     "rmagatti/auto-session",
