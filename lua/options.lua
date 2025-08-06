e9f73d2e20125aed65295e89b8a701be94f37cb7
Le Tan Dang Khoa <letan.dangkhoa@gmail.com>
2025-08-06 08:11:40 +0000

    fix: remove help out of autosession

--- lua/options.lua
+++ lua/options.lua
@@ -1,7 +1,7 @@
 require "nvchad.options"
 
 -- session config
-vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+vim.o.sessionoptions = "blank,buffers,curdir,folds,tabpages,winsize,winpos,terminal,localoptions"
 
 -- line number
 vim.o.number = false
