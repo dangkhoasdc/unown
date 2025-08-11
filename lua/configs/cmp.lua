71796fe53b798219953dedd0f2f7581c3e23a59f
Le Tan Dang Khoa <letan.dangkhoa@gmail.com>
2025-08-11 02:43:02 +0000

    fix: removing annoying Tab/S-Tab for completion navigation

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -1,34 +1,14 @@
 -- cmp setting
 local cmp = require "cmp"
 local config = cmp.get_config()
-local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-    return false
-  end
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match "^%s*$" == nil
-end
 
 table.insert(config.sources, {
   name = "copilot",
   group_index = 1,
 })
-config.mapping = cmp.mapping.preset.insert {
-  ["<Tab>"] = cmp.mapping(function(fallback)
-    if cmp.visible() and has_words_before() then
-      cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
-    else
-      fallback()
-    end
-  end, { "i", "s" }),
-  ["<S-Tab>"] = cmp.mapping(function(fallback)
-    if cmp.visible() then
-      cmp.select_prev_item()
-    else
-      fallback()
-    end
-  end, { "i", "s" }),
-}
+
+config.mapping["Tab"] = nil
+config.mapping["<S-Tab>"] = nil
 
 config.formatting = {
   format = function(entry, vim_item)
