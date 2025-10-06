acd4d772b8426c9614f68cefdec36d6c6e3c81dc
Le Tan Dang Khoa <letan.dangkhoa@gmail.com>
2025-10-06 09:19:13 +0000

    feat: add preview refs/defs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -59,6 +59,18 @@ return {
     },
   },
 
+  -- lsp preview windows
+  {
+    "rmagatti/goto-preview",
+    dependencies = { "rmagatti/logger.nvim" },
+    event = "BufEnter",
+    config = function()
+      require('goto-preview').setup {
+        default_mappings = true,
+      }
+    end,
+  },
+
   -- formatters
   {
     "stevearc/conform.nvim",
