de1037e89c86e15567e19bece77fb18207d30bc4
Le Tan Dang Khoa <letan.dangkhoa@gmail.com>
2025-10-06 08:56:32 +0000

    fix: arrow index keys

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -345,6 +345,7 @@ return {
       show_icons = true,
       leader_key = ";",        -- Recommended to be a single key
       buffer_leader_key = "m", -- Per Buffer Mappings
+      index_keys = "1234789zxcbnafghjklwrtyuiop",
     },
   },
   {
