2cc751d8d80f43fa0d26f91ab20c646112edc6de
Le Tan Dang Khoa <letan.dangkhoa@gmail.com>
2025-12-26 13:03:31 +0800

    fix: no blending for Undo window

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -392,7 +392,9 @@ return {
     ---@module 'undotree.collector'
     ---@type UndoTreeCollector.Opts
     opts = {
-      -- your options
+      window = {
+        winblend = 0
+      }
     },
     keys = { -- load the plugin only when using it's keybinding:
       { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
