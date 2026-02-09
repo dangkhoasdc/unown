9a5306a942f7569f37831d5b00efe6970386c8e4
Le Tan Dang Khoa <letan.dangkhoa@gmail.com>
2026-02-09 05:17:03 +0000

    fix: highlight some missing builtin types

---
+++ after/queries/d/highlights.scm
@@ -0,0 +1,12 @@
+; extends
+
+; Aliased builtin types from druntime/object.d
+; These are grammar nodes (not identifiers), highlight as builtin
+[
+  (string)
+  (wstring)
+  (dstring)
+  (size_t)
+  (ptrdiff_t)
+  (noreturn)
+] @type.builtin
